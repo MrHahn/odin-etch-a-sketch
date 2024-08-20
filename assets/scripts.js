9643cc1ab5458ea6dc00e74610019a77ab873546
Christopher Hahn <chahn970@gmail.com>
2024-08-20 17:48:37 -0600

    complete first iteration of the drawing function

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,10 +1,27 @@
+const grid = document.querySelector('#grid-container');
+let gridItems = [];
+
+
 function createGrid(num){
-    const grid = document.querySelector('#grid-container');
     for(let i = 0; i < num; i++){
         let gridItem = document.createElement('div');
         gridItem.classList.add('grid-item');
         grid.appendChild(gridItem);
+        gridItems.push(gridItem);
+        
     } 
 }
-
 createGrid(256);
+
+
+function changeOnHover(){
+    console.log(this);
+    this.classList.add('hovered');
+}
+
+
+
+ gridItems.forEach((item) => {
+    item.addEventListener('mouseover', changeOnHover )
+ });
+
