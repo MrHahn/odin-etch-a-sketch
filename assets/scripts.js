451a98543c23cda0117827bc81599e5123a170fb
Christopher Hahn <chahn970@gmail.com>
2024-08-20 16:38:27 -0600

    create initial grid

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,1 +1,10 @@
-console.log('working');
+function createGrid(num){
+    const grid = document.querySelector('#grid-container');
+    for(let i = 0; i < num; i++){
+        let gridItem = document.createElement('div');
+        gridItem.classList.add('grid-item');
+        grid.appendChild(gridItem);
+    } 
+}
+
+createGrid(256);
