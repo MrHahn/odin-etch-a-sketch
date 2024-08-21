2c19ed5712ea980358918b7d2a1370213e5ba014
Christopher Hahn <chahn970@gmail.com>
2024-08-20 21:30:19 -0600

    add reset button and functionality

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,35 +1,45 @@
 const grid = document.querySelector('#grid-container');
 let gridItems = [];
 let userNum = prompt("How many squares per side?")
-
-if(userNum > 100){
-    userNum = prompt("thats too many, please enter a number 100 or lower")
-}else{
-    let gridSize = userNum * userNum;
-    createGrid(gridSize, userNum);
-}
+let resetBtn = document.querySelector('.reset-grid');
 
 
+prepareGrid(userNum);
 
- gridItems.forEach((item) => {
-    item.addEventListener('mouseover', changeOnHover )
- });
+
+ resetBtn.addEventListener('click', () => {
+    userNum = prompt("please enter a number 100 or lower");
+    prepareGrid(userNum);
+ })
 
 
  function createGrid(num, userNum){
+    gridItems.length = 0;
+    grid.innerHTML = '';
     let boxWidth = (960 / userNum) - 2; // subtracting the borders on each side
-    grid.innerHTML == '';
     for(let i = 0; i < num; i++){
         let gridItem = document.createElement('div');
         gridItem.classList.add('grid-item');
         gridItem.style.width = boxWidth + 'px';
         gridItem.style.height = boxWidth + 'px';
         grid.appendChild(gridItem);
-        gridItems.push(gridItem);
-        
+        gridItems.push(gridItem);  
     } 
+    gridItems.forEach((item) => {
+        item.addEventListener('mouseover', changeOnHover )
+     });
 }
+
  function changeOnHover(){
     this.classList.add('hovered');
+}
+
+function prepareGrid(userNum){
+    if(userNum > 100){
+        userNum = prompt("thats too many, please enter a number 100 or lower")
+    }else{
+        let gridSize = userNum * userNum;
+        createGrid(gridSize, userNum);
+    }
 }
 
