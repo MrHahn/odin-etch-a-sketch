316500e6eeb3e6ec2ac21ecf38c3b8ec9db3b1fe
Christopher Hahn <chahn970@gmail.com>
2024-08-20 20:55:59 -0600

    complete second iteration of drawing function with changing grid size

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,22 +1,12 @@
 const grid = document.querySelector('#grid-container');
 let gridItems = [];
+let userNum = prompt("How many squares per side?")
 
-
-function createGrid(num){
-    for(let i = 0; i < num; i++){
-        let gridItem = document.createElement('div');
-        gridItem.classList.add('grid-item');
-        grid.appendChild(gridItem);
-        gridItems.push(gridItem);
-        
-    } 
-}
-createGrid(256);
-
-
-function changeOnHover(){
-    console.log(this);
-    this.classList.add('hovered');
+if(userNum > 100){
+    userNum = prompt("thats too many, please enter a number 100 or lower")
+}else{
+    let gridSize = userNum * userNum;
+    createGrid(gridSize, userNum);
 }
 
 
@@ -24,4 +14,22 @@ function changeOnHover(){
  gridItems.forEach((item) => {
     item.addEventListener('mouseover', changeOnHover )
  });
+
+
+ function createGrid(num, userNum){
+    let boxWidth = (960 / userNum) - 2; // subtracting the borders on each side
+    grid.innerHTML == '';
+    for(let i = 0; i < num; i++){
+        let gridItem = document.createElement('div');
+        gridItem.classList.add('grid-item');
+        gridItem.style.width = boxWidth + 'px';
+        gridItem.style.height = boxWidth + 'px';
+        grid.appendChild(gridItem);
+        gridItems.push(gridItem);
+        
+    } 
+}
+ function changeOnHover(){
+    this.classList.add('hovered');
+}
 
